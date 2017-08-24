bf0cdbce8cd185ea8e4c621a38bbe6556996cbb8
Oscar Godoy <ogodoy.re@gmail.com>
2017-08-24 08:15:08 +0200

    starting with three species

--- code/Structural coexistence.R
+++ code/Structural coexistence.R
@@ -5,6 +5,8 @@
 #and Jonathan M. Levine
 #published in: Ecological Monographs
 
+#No LUI with 3sp----
+
 rm(list=ls())
 source('code/toolbox_coexistence.R')
 source('code/toolbox_figure.R')
@@ -13,7 +15,7 @@ source('code/toolbox_figure.R')
 alpha <- as.matrix(read.table("results/interaction_matrix_50.csv", header=T, sep=",", row.names=1))
 intrinsic <- as.matrix(read.table("results/intrinsic.csv", header=T, sep=",", row.names=1))
 
-#calculate all possible combinations of three and four species. 
+#calculate all possible combinations of three species. 
 alpha <- alpha*-1 # change the sign as required in this framework positive interactions mean competition.
 # Important! Serguei Saavedra confirmed that the framework only works for this species combinations in which intraspecific effects are all of the same sign. 
 
@@ -21,13 +23,13 @@ alpha <- alpha*-1 # change the sign as required in this framework positive inter
 combos3 <- t(combn(rownames(alpha),3))
 nd_fd3 <- matrix(nrow=dim(combos3)[1], ncol=6)
 row.names(nd_fd3) <- apply(combos3,1,paste,collapse=".") 
-colnames(nd_fd3) <- c("omega", "theta", "feasibility","coexisting pairs1","coexisting pairs2","coexisting pairs3") 
+colnames(nd_fd3) <- c("omega", "theta", "feasibility","coexisting pairs1:2","coexisting pairs1:3","coexisting pairs2:3") 
 
 # It serves to check other properties of the coexisting species. 
 #"centroid", "feasibility_pairs")
 
 #this loop can not calculate outcomes for two combinations (number 654 and 807)
-for(i in 808:dim(combos3)[1]){
+for(i in 1:dim(combos3)[1]){
   alpha2 <- alpha[combos3[i,], combos3[i,]]
   nd_fd3[i,1] <- Omega(alpha2)
   intrinsic2 <- intrinsic[combos3[i,],]
@@ -41,7 +43,7 @@ for(i in 808:dim(combos3)[1]){
 #Remove NA cases
 nd_fd3 <- as.data.frame(nd_fd3[complete.cases(nd_fd3), ])
 
-#Remove -Inf Omega values
+#Remove -Inf Omega values that occur when the intras has negative and positive values
 nd_fd3 <- subset(nd_fd3, omega > -Inf)
 hist(nd_fd3$omega)
 summary(nd_fd3$omega)
@@ -50,18 +52,39 @@ summary(nd_fd3$omega)
 nd_fd3.2 <- nd_fd3
 nd_fd3.2<-within(nd_fd3.2, omega[omega<0] <- 0)
 hist(nd_fd3.2$omega)
+summary(nd_fd3.2$omega)
+
+par(mfrow=c(1,2))
+hist(nd_fd3.2$omega) # these are the niche differences
+hist(nd_fd3.2$theta) # these are the fitness differences
 
 #Separate data.frame between the coexisting and the non-coexisting triplets
 coex_triplets <- subset(nd_fd3.2, feasibility ==1)
 non_coex_triplets <- subset(nd_fd3.2, feasibility ==0)
 
+#Which of these triplets coexist due to intransitivity
+intransitivity <- subset(coex_triplets, coex_triplets$`coexisting pairs1:2`==0 & coex_triplets$`coexisting pairs1:3`==0 & coex_triplets$`coexisting pairs2:3`==0)
+
+#Which of these triplets coexist when also some species pair coexist (diffuse competition)
+diffuse <- subset(coex_triplets, !(coex_triplets$`coexisting pairs1:2`==0 & coex_triplets$`coexisting pairs1:3`==0 & coex_triplets$`coexisting pairs2:3`==0))
+diffuse <- subset(diffuse, !(diffuse$`coexisting pairs1:2`==1 & diffuse$`coexisting pairs1:3`==1 & diffuse$`coexisting pairs2:3`==1))
+
+#Which of these triplets coexist when also the three pairs coexist. 
+all_coex_pairs<- subset(coex_triplets, coex_triplets$`coexisting pairs1:2`==1 & coex_triplets$`coexisting pairs1:3`==1 & coex_triplets$`coexisting pairs2:3`==1)
+       
+
 #This is to remove cases with no niche differences
 #row_sub = apply(nd_fd3.2, 1, function(row) all(row !=0 ))
 #nd_fd3.2<- nd_fd3.2[row_sub,]
 
-par(mfrow=c(1,2))
-hist(nd_fd3.2$omega) # these are the niche differences
-hist(nd_fd3.2$theta) # these are the fitness differences
+#Low LUI with 3sp----
+rm(list=ls())
+source('code/toolbox_coexistence.R')
+source('code/toolbox_figure.R')
+
+#3-species interaction matrix of figure 5 and 6
+alpha <- as.matrix(read.table("results/interaction_matrix_50.csv", header=T, sep=",", row.names=1))
+intrinsic <- as.matrix(read.table("results/intrinsic.csv", header=T, sep=",", row.names=1))
 
 
 
