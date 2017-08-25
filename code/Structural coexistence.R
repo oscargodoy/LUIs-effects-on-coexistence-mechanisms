8a6c62480dcbd706bb0a9b7fe8314e6cf7587fe9
Oscar Godoy <ogodoy.re@gmail.com>
2017-08-25 18:37:24 +0200

    working on the code

--- code/Structural coexistence.R
+++ code/Structural coexistence.R
@@ -4,83 +4,76 @@
 #Serguei Saavedra, Rudolf P. Rohr, Jordi Bascompte, Oscar Godoy, Nathan J.B. Kraft,
 #and Jonathan M. Levine. Published in: Ecological Monographs
 
+# Important! Serguei Saavedra and Rudolf Rohr confirmed that the framework only works with intraspecific positive values meaning all species experience some sort of intraspecific competition.
+# So after sorting species alpha matrices needs to be multiply by -1
+
 rm(list=ls())
 source('code/toolbox_coexistence.R')
 source('code/toolbox_figure.R')
 
 #loading average values
 alpha <- as.matrix(read.table("results/interaction_matrix_lme_average_50.csv", header=T, sep=",", row.names=1))
-intrinsic <- as.matrix(read.table("results/intrinsic_site_lui_average_lme_50.csv", header=T, sep=",", row.names=1))
+intrinsic <- read.table("results/intrinsic_site_lui_average_lme_50.csv", header=T, sep=",", row.names=1)
 
+#calculate average intrinsic growth rates across three sites
+intrinsic$SiteA <- intrinsic$Intrinsic
+intrinsic$SiteH <- intrinsic$Intrinsic + intrinsic$SiteH
+intrinsic$SiteS <- intrinsic$Intrinsic + intrinsic$SiteS
+lui <- intrinsic$LUI
+intrinsic <- intrinsic[,-c(1,4)]
+intrinsic$Site_average <- apply(intrinsic,1,mean)
+intrinsic$lui <-lui
 
 #loading std.error values
 alpha.error <- as.matrix(read.table("results/interaction_matrix_lme_std_error_50.csv", header=T, sep=",", row.names=1))
-
-# Important! Serguei Saavedra and Rudolf Rohr confirmed that the framework only works with intraspecific positive values meaning all species experience some sort of intraspecific competition.
-#Therefore lets make all calculations with high and low LUI and then multiply by -1. 
-
-diag(alpha)>0 
-# Result: Three sps in the alpha matrix needs to be removed at average values because they have intra facilitation: Urt_dio, Car_car, Ely_rep.
-
 alpha_95_low <- alpha * (-1.96*alpha.error)
 diag(alpha_95_low)>0 
 # Result: Same as before, three sps in the alpha matrix needs to be removed Urt_dio, Car_car, Ely_rep.
-
 alpha_95_high <- alpha * (1.96*alpha.error)  
 diag(alpha_95_high)>0
 #This is the opposite these three species have now negative values. 
 # as CI 95% can not be evaluated because of positive values yet they are symmetrical we will evaluate only at 95% of one of the sides.
-
-#Final set of 48 sps
-names<-c("Ely_rep", "Car_car", "Urt_dio")
-alpha<- alpha[-which(rownames(alpha) %in% names),-which(colnames(alpha) %in% names)]
-alpha_95_high<- alpha_95_high[-which(rownames(alpha_95_high) %in% names),-which(colnames(alpha_95_high) %in% names)]
 rm(alpha_95_low)
 
-#How the intras in the matrix are modified when include low and high LUI
-#Load the data without the three species removed
-lui <- as.matrix(read.table("results/lui_matrix_lme_average_50.csv", header=T, sep=",", row.names=1))
-lui<- lui[-which(rownames(lui) %in% names),-which(colnames(lui) %in% names)]
-lui.error <- as.matrix(read.table("results/lui_matrix_lme_std_error_50.csv", header=T, sep=",", row.names=1))
-lui.error<- lui.error[-which(rownames(lui.error) %in% names),-which(colnames(lui.error) %in% names)]
+#Coexistence no LUI----
 
-#evaluate the effect of LUI
-#low LUI set at 0.5
-  #Average and error
-alpha_low_lui <- alpha - (lui*0.5)
-alpha_low_lui_95_high <- alpha_95_high - (lui.error*0.5*1.96)
+#We need to perform some steps
 
-#High LUI set at 3.5
-#Average and error
-alpha_high_lui<- alpha - (lui*3.5)
-alpha_low_lui_95_high <- alpha_95_high - (lui.error*3.5*1.96)
+#Step1: Which species can not coexist without LUI (negative intrinsic growth rates)
 
+sp_no_lui <- row.names(subset(intrinsic, Site_average>0))
 
-#Check that with low and high LUI we are ok of all values in the diagonal being competition (negative values so far), remember to change to positive before performing analyses
-diag(alpha_low_lui)<0
-diag(alpha_high_lui)<0 
+#subset in the alpha matrix and in the intrinsic growth rate matrix
 
+alpha<- alpha[which(rownames(alpha) %in% sp_no_lui),which(colnames(alpha) %in% sp_no_lui)]
+alpha_95_high<- alpha_95_high[which(rownames(alpha_95_high) %in% sp_no_lui),which(colnames(alpha_95_high) %in% sp_no_lui)]
 
-#calculate all possible combinations of three and four species.
+#Step2: Which species can not be incorporated into the models due to facilitative intra effects
+diag(alpha)>0 
+# Result: Two sps in the alpha matrix needs to be removed at average values because they have intra facilitation: Urt_dio, Car_car
+names<-c("Car_car", "Urt_dio")
+alpha<- alpha[-which(rownames(alpha) %in% names),-which(colnames(alpha) %in% names)]
+alpha_95_high<- alpha_95_high[-which(rownames(alpha_95_high) %in% names),-which(colnames(alpha_95_high) %in% names)]
+
+# A total set of 31 species to work with NO LUI, the number os species varies as LUI increases.
+
+#Three species
 combos3 <- t(combn(rownames(alpha),3))
-combos4 <- t(combn(rownames(alpha),4))
-
-#No LUI with 3sp----
-
-#All combination of three species
+#combos4 <- t(combn(rownames(alpha),4))
 
 nd_fd3 <- matrix(nrow=dim(combos3)[1], ncol=6)
 row.names(nd_fd3) <- apply(combos3,1,paste,collapse=".") 
 colnames(nd_fd3) <- c("omega", "theta", "feasibility","coexisting pairs1:2","coexisting pairs1:3","coexisting pairs2:3") 
 
-# It serves to check other properties of the coexisting species. 
-#"centroid", "feasibility_pairs")
+#multipy by -1
+alpha <- alpha*-1
+alpha_95_high <- alpha_95_high*-1
 
 #this loop can not calculate outcomes for two combinations (number 654 and 807)
 for(i in 1:dim(combos3)[1]){
   alpha2 <- alpha[combos3[i,], combos3[i,]]
-  nd_fd3[i,1] <- Omega(alpha2)
-  intrinsic2 <- intrinsic[combos3[i,],]
+  nd_fd3[i,1] <- exp(Omega(alpha2))
+  intrinsic2 <- intrinsic[combos3[i,],] ## This needs to be a vector of Site Average, now is a matrix !!!!!!
   nd_fd3[i,2] <- theta(alpha2,intrinsic2)
   nd_fd3[i,3] <- test_feasibility(alpha2,intrinsic2)
   x <- test_feasibility_pairs(alpha2,intrinsic2)
@@ -119,7 +112,46 @@ diffuse <- subset(diffuse, !(diffuse$`coexisting pairs1:2`==1 & diffuse$`coexist
 
 #Which of these triplets coexist when also the three pairs coexist. 
 all_coex_pairs<- subset(coex_triplets, coex_triplets$`coexisting pairs1:2`==1 & coex_triplets$`coexisting pairs1:3`==1 & coex_triplets$`coexisting pairs2:3`==1)
-       
+
+
+
+
+
+
+
+
+#How the intras in the matrix are modified when include low and high LUI
+#Load the data without the three species removed
+lui <- as.matrix(read.table("results/lui_matrix_lme_average_50.csv", header=T, sep=",", row.names=1))
+lui<- lui[-which(rownames(lui) %in% names),-which(colnames(lui) %in% names)]
+lui.error <- as.matrix(read.table("results/lui_matrix_lme_std_error_50.csv", header=T, sep=",", row.names=1))
+lui.error<- lui.error[-which(rownames(lui.error) %in% names),-which(colnames(lui.error) %in% names)]
+
+#evaluate the effect of LUI
+#low LUI set at 0.5
+  #Average and error
+alpha_low_lui <- alpha + (lui*0.5) #always is + no -
+alpha_low_lui_95_high <- alpha_95_high - (lui.error*0.5*1.96)
+
+#High LUI set at 3.5
+#Average and error
+alpha_high_lui<- alpha + (lui*3.5)
+alpha_low_lui_95_high <- alpha_95_high - (lui.error*3.5*1.96)
+
+
+#Check that with low and high LUI we are ok of all values in the diagonal being competition (negative values so far), remember to change to positive before performing analyses
+diag(alpha_low_lui)<0
+diag(alpha_high_lui)<0 
+
+
+#calculate all possible combinations of three and four species.
+combos3 <- t(combn(rownames(alpha),3))
+combos4 <- t(combn(rownames(alpha),4))
+
+#No LUI with 3sp----
+
+#All combination of three species
+
 
 #This is to remove cases with no niche differences
 #row_sub = apply(nd_fd3.2, 1, function(row) all(row !=0 ))
